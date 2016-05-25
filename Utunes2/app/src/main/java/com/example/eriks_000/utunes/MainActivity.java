d44e946800966d39d2647bb8e4a77f48b3d8f4e2
ErikSeger <erik.segerstrom91@gmail.com>
2016-05-25 19:44:41 +0200

    final push small changes

--- Utunes2/app/src/main/java/com/example/eriks_000/utunes/MainActivity.java
+++ Utunes2/app/src/main/java/com/example/eriks_000/utunes/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawer;
@@ -18,11 +19,11 @@ public class MainActivity extends AppCompatActivity {
     private NavigationView nvDrawer;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         // Set a Toolbar to replace the ActionBar.
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
--- Utunes2/app/src/main/java/com/example/eriks_000/utunes/StoreFragment.java
+++ Utunes2/app/src/main/java/com/example/eriks_000/utunes/StoreFragment.java
@@ -2,15 +2,20 @@ package com.example.eriks_000.utunes;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
 /**
  * Created by eriks_000 on 2016-05-25.
  */
-public class StoreFragment extends Fragment {
+public class StoreFragment extends Fragment implements OnClickListener {
 
+
+    Button button;
     public StoreFragment()
     {
 
@@ -20,8 +25,24 @@ public class StoreFragment extends Fragment {
     {
         View storeView = inflater.inflate(R.layout.fragment_store, container, false);
 
+        button = (Button)storeView.findViewById(R.id.nav_Disco);
+        button.setOnClickListener(this);
+
+
+
         return storeView;
     }
 
+
+    @Override
+    public void onClick(View v) {
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch (v.getId()) {
+            case R.id.nav_Disco:
+                fragmentClass = SongListFragment.class;
+                break;
+        }
+    }
 
 }
