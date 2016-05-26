8f082004ab69c62a65091b24feb44c760fc57687
ErikSeger <erik.segerstrom91@gmail.com>
2016-05-26 12:37:44 +0200

    Revert "Implimented albumlist currently somewhat buggy"
    This reverts commit 101a42d43060bf000c5fe2a2dbe1734cfa5221e1.
    
    # Conflicts:
    #	Utunes2/app/src/main/java/com/example/eriks_000/utunes/AlbumItem.java

--- Utunes2/app/src/main/java/com/example/eriks_000/utunes/SongListFragment.java
+++ Utunes2/app/src/main/java/com/example/eriks_000/utunes/SongListFragment.java
@@ -1,10 +1,10 @@
 package com.example.eriks_000.utunes;
 
-import android.support.v4.app.Fragment;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+        import android.support.v4.app.Fragment;
+        import android.os.Bundle;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
 
 /**
  * Created by eriks_000 on 2016-05-25.
